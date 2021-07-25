e2e5499116c97972180438e387066f4cd0976690
Michael Berg <michael.berg@lovoo.com>
2021-07-26 00:21:39 +0200

    #1363 prepare GradientStrokeRenderer

--- lottie-swift/src/Private/NodeRenderSystem/Nodes/OutputNodes/Renderables/GradientStrokeRenderer.swift
+++ lottie-swift/src/Private/NodeRenderSystem/Nodes/OutputNodes/Renderables/GradientStrokeRenderer.swift
@@ -17,43 +17,18 @@ final class GradientStrokeRenderer: PassThroughOutputNode, Renderable {
     return updates || strokeRender.hasUpdate || gradientRender.hasUpdate
   }
   
-  var shouldRenderInContext: Bool = true
-  
-  func updateShapeLayer(layer: CAShapeLayer) {
-    /// Not Applicable
-  }
-  
   let strokeRender: StrokeRenderer
   let gradientRender: GradientFillRenderer
   
   override init(parent: NodeOutput?) {
-    self.strokeRender  = StrokeRenderer(parent: nil)
-    self.gradientRender = GradientFillRenderer(parent: nil)
+    self.strokeRender  = StrokeRenderer(parent: parent)
+    self.gradientRender = GradientFillRenderer(parent: parent)
     self.strokeRender.color = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1, 1, 1, 1])
     super.init(parent: parent)
   }
-  
-  func render(_ inContext: CGContext) {
-    guard inContext.path != nil && inContext.path!.isEmpty == false else {
-      return
-    }
 
-    strokeRender.hasUpdate = false
-    hasUpdate = false
-    gradientRender.hasUpdate = false
-    
-    strokeRender.setupForStroke(inContext)
-    
-    inContext.replacePathWithStrokedPath()
-    
-    /// Now draw the gradient.
-    gradientRender.render(inContext)
-
+  func updateShapeLayer(layer: CAShapeLayer) {
+//    gradientRender.updateShapeLayer(layer: layer)
+    strokeRender.updateShapeLayer(layer: layer)
   }
-  
-  func renderBoundsFor(_ boundingBox: CGRect) -> CGRect {
-    return strokeRender.renderBoundsFor(boundingBox)
-  }
-  
-  
 }
